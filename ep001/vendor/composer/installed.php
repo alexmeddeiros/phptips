8c761f2396e47d165b1676a4e714b5a88a89ce61
Alex Medeiros <alexmeddeiros@gmail.com>
2020-11-13 23:32:18 -0500

    datalayer

---
+++ ep001/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '75d382ab0f3033bf505f674fb7f6d848f7098864',
+    'name' => 'alexmeddeiros/phptips',
+  ),
+  'versions' => 
+  array (
+    'alexmeddeiros/phptips' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '75d382ab0f3033bf505f674fb7f6d848f7098864',
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.0.7',
+      'version' => '6.0.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0c41a36d4508d470e376498c1c0c527aa36a2d59',
+    ),
+  ),
+);
---
+++ ep002/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '75d382ab0f3033bf505f674fb7f6d848f7098864',
+    'name' => 'alexmeddeiros/phptips',
+  ),
+  'versions' => 
+  array (
+    'alexmeddeiros/phptips' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '75d382ab0f3033bf505f674fb7f6d848f7098864',
+    ),
+    'coffeecode/datalayer' => 
+    array (
+      'pretty_version' => '1.1.2',
+      'version' => '1.1.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '928012861d39b855c91485a0f1bd29d202c2fd08',
+    ),
+  ),
+);
