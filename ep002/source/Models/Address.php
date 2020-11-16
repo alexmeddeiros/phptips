8000bacd002bcda23a458af668fae262e247a152
Alex Medeiros <alexmeddeiros@gmail.com>
2020-11-16 01:28:44 -0500

    abstração

---
+++ ep002/source/Models/Address.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Source\Models;
+
+use CoffeeCode\DataLayer\DataLayer;
+
+class Address extends DataLayer
+{
+    public function __construct()
+    {
+        parent::__construct("address", ["user_id"], "addr_id","false");
+    }
+
+
+    public function add(User $user, string $street, string $number) : address
+    {
+        $this->user_id = $user->id;
+        $this->street = $street;
+        $this->number = $number;
+
+        return $this;
+    }
+}
